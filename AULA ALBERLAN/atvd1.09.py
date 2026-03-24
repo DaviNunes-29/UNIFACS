f66f390957ce156407f4bd47dcb203e444328a92
davinunesxz <nunesdavi2912@gmail.com>
2026-03-24 19:47:32 -0300

    atividades

---
+++ AULA ALBERLAN/atvd1.09.py
@@ -0,0 +1,6 @@
+Farenheit = float(input("Informe sua temperatura em graus Farenheit:"))
+
+Celsius = (5 * (Farenheit-32) / 9)
+
+print("A temperatura em grus celcius é de:", Celsius,"°")
+
---
+++ AULA ALBERLAN/atvd1.10.py
@@ -0,0 +1,5 @@
+Celsius = float(input("Informe a temperatura em graus celcius"))
+
+Farenheit = (Celsius * 9/5) + 32 
+
+print("A temperatura em graus Farenheit é de:", Farenheit,"°")
