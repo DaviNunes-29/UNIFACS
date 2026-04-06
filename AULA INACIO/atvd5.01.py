bd22e8d3bbec420ba4354acad4573ff2ef65259c
davinunesxz <nunesdavi2912@gmail.com>
2026-04-06 16:18:12 -0300

    atvd05

---
+++ AULA INACIO/atvd5.01.py
@@ -0,0 +1,9 @@
+lista = []
+
+for i in range(5):
+    numero = int(input("Digite um número: "))
+    lista.append(numero)
+
+lista.sort()
+
+print("Lista em ordem crescente:", lista)
---
+++ AULA INACIO/atvd5.04.py
@@ -0,0 +1,14 @@
+matriz = []
+
+for i in range(3):
+    linha = []
+    for j in range(3):
+        valor = int(input(f"Digite o valor para [{i}][{j}]: "))
+        linha.append(valor)
+    matriz.append(linha)
+
+soma = 0
+for i in range(3):
+    soma += matriz[i][i]
+
+print("Soma da diagonal principal:", soma)
